f41715b719411ec6aaf5d03f19bd2a8cd3a785ad
Oguzhan <yakusio@googlemail.com>
2023-03-26 15:40:32 +0200

    * Added unary minus operator and equality Operator overloading

---
+++ Section14Challange/Section14Challange/Mystring.cpp
@@ -0,0 +1,114 @@
+#include <iostream>
+#include <cstring>
+#include "Mystring.h"
+#pragma warning(disable:4996) // Visual studio doesn't allow strcpy for unsafety (Workaround)
+
+// No-args constructor
+Mystring::Mystring()
+    : str{ nullptr } {
+    str = new char[1];
+    *str = '\0';
+}
+
+// Overloaded constructor
+Mystring::Mystring(const char* s)
+    : str{ nullptr } {
+    if (s == nullptr) {
+        str = new char[1];
+        *str = '\0';
+    }
+    else {
+        str = new char[strlen(s) + 1];
+        std::strcpy(str, s);
+    }
+}
+
+// Copy constructor
+Mystring::Mystring(const Mystring& source)
+    : str{ nullptr } {
+    str = new char[strlen(source.str) + 1];
+    std::strcpy(str, source.str);
+    //       std::cout << "Copy constructor used" << std::endl;
+
+}
+
+// Move constructor
+Mystring::Mystring(Mystring&& source)
+    :str(source.str) {
+    source.str = nullptr;
+    //        std::cout << "Move constructor used" << std::endl;
+}
+
+// Destructor
+Mystring::~Mystring() {
+    delete[] str;
+}
+
+// Copy assignment
+Mystring& Mystring::operator=(const Mystring& rhs) {
+    //    std::cout << "Using copy assignment" << std::endl;
+
+    if (this == &rhs)
+        return *this;
+    delete[] str;
+    str = new char[strlen(rhs.str) + 1];
+    std::strcpy(str, rhs.str);
+    return *this;
+}
+
+// Move assignment
+Mystring& Mystring::operator=(Mystring&& rhs) {
+    //   std::cout << "Using move assignment" << std::endl;
+    if (this == &rhs)
+        return *this;
+    delete[] str;
+    str = rhs.str;
+    rhs.str = nullptr;
+    return *this;
+}
+
+
+// Display method
+void Mystring::display() const {
+    std::cout << str << " : " << get_length() << std::endl;
+}
+
+// getters
+int Mystring::get_length() const { return strlen(str); }
+const char* Mystring::get_str() const { return str; }
+
+
+
+// overloaded insertion operator
+std::ostream& operator<<(std::ostream& os, const Mystring& rhs) {
+    os << rhs.str;
+    return os;
+}
+
+// overloaded extraction operator
+std::istream& operator>>(std::istream& in, Mystring& rhs) {
+    char* buff = new char[1000];
+    in >> buff;
+    rhs = Mystring{ buff };
+    delete[] buff;
+    return in;
+}
+
+/*Overloaded operators*/
+Mystring Mystring::operator-() const
+{
+    char* tempString = new char[std::strlen(str)+1];
+    std::strcpy(tempString, str);
+    for (size_t numChar{ 0 }; numChar < std::strlen(str); ++numChar)
+    {
+        tempString[numChar] = std::tolower(str[numChar]);
+    }
+    Mystring tempObj{ tempString };
+    delete[] tempString;
+    return tempObj;
+}
+
+bool Mystring::operator==(const Mystring rhs) const
+{
+    return (std::strcmp(this->str, rhs.str) == 0);
+}
